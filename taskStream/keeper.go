7e5038adc72b3667b11ffa68cbf540c042cdd7cf
Marko Baricevic <>
2019-01-01 21:09:09 +0100

    task adjustment

--- taskStream/keeper.go
+++ taskStream/keeper.go
@@ -10,9 +10,10 @@ import (
 type Keeper struct {
 	coinKeeper bank.Keeper
 
-	authorsKey sdk.StoreKey
-	valueKey   sdk.StoreKey
-	taskKey    sdk.StoreKey
+	ownersKey   sdk.StoreKey
+	valueKey    sdk.StoreKey
+	taskKey     sdk.StoreKey
+	finishedKey sdk.StoreKey
 
 	cdc *codec.Codec
 }
@@ -22,8 +23,12 @@ func (k Keeper) createTask(ctx sdk.Context, taskName string, value string) {
 	store.Set([]byte(taskName), []byte(value))
 }
 
-func (k Keeper) solveTask(ctx sdk.Context, taskName string) string {
-	store := ctx.KVStore(k.taskKey)
-	bz := store.Get([]byte(taskName))
-	return string(bz)
+func (k Keeper) solveTask(ctx sdk.Context, taskName string, value string, solver sdk.AccAddress) {
+	store := ctx.KVStore(k.finishedKey)
+	store.Set([]byte(taskName), solver)
+}
+
+func (k Keeper) setOwner(ctx sdk.Context, taskName string, taker sdk.AccAddress) {
+	store := ctx.KVStore(k.ownersKey)
+	store.Set([]byte(taskName), taker)
 }
