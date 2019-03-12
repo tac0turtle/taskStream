0efb6c282f20cf31876f32079232e9b2b82ea72c
Marko Baricevic <marbar3778@yahoo.com>
2019-03-12 15:50:14 +0100

    cleanup and setup of simpler fashion

---
+++ types/task.go
@@ -0,0 +1,19 @@
+package types
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+type Task struct {
+	TaskTitle       string           `json:"task_title"`
+	TaskDescription string           `json:"task_description"`
+	Backers         []sdk.AccAddress `json:"backers"`
+	Value           sdk.Coins        `json:"value`
+}
+
+func createTask(taskTitle string, taskDescription string, backers []sdk.AccAddress, value sdk.Coins) Task {
+	return Task{
+		TaskTitle:       taskTitle,
+		TaskDescription: taskDescription,
+		Backers:         backers,
+		Value:           value,
+	}
+}
---
+++ x/taskstreamer/keeper.go
@@ -0,0 +1,57 @@
+package taskstreamer
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	taskType "github.com/marbar3778/taskStream/types"
+)
+
+type Keeper struct {
+	coinKeeper bank.Keeper
+
+	taskStore sdk.StoreKey
+
+	cdc *codec.Codec
+}
+
+func NewKeeper(coinKeeper bank.Keeper, taskStore sdk.StoreKey, cdc *codec.Codec) Keeper {
+	return Keeper{
+		coinKeeper: coinKeeper,
+		taskStore:  taskStore,
+		cdc:        cdc,
+	}
+}
+
+func (k Keeper) GetTask(ctx sdk.Context, taskTitle string) taskType.Task {
+	store := ctx.KVStore(k.taskStore)
+	if !store.Has([]byte(name)) {
+		return
+	}
+	task := store.Get([]byte(taskName))
+	var taskDetails taskType.Task
+	k.cdc.MarshalBinaryBare(task, &taskDetails)
+	return taskDetails
+}
+
+func (k Keeper) SetTask(ctx sdk.Context, taskTitle string, taskData taskType.Task) {
+	store := ctx.KVStore(k.taskStore)
+	store.Set([]byte(taskName), []byte(taskData))
+}
+
+func (k Keeper) CreateTask(ctx sdk.Context, taskTitle string, taskDescription string, backers []sdk.AccAddress, value sdk.Coins) {
+	task := taskType.CreateTask(taskTitle, taskDescription, backers, value)
+	k.SetTask(ctx, taskTitle, task)
+}
+
+func (k Keeper) BecomeBacker(ctx sdk.Context, taskTitle string, newBacker sdk.AccAddress, addedValue sdk.Coins) {
+	task := k.GetTask(ctx, taskTitle)
+	task.Value + addedValue
+	task.Backers = append(task.Backers, newBacker)
+	k.SetTask(ctx, taskTitle, task)
+}
+
+// func (k Keeper) PayoutTask(ctx sdk.Context, taskTitle string, receiver sdk.AccAddress) {
+
+// }
